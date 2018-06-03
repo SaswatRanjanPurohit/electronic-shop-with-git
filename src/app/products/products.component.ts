6d5d3a026778812ad7e5df79b8767688c61932e4
saswat <saswat333@gmail.com>
2018-06-03 12:10:58 +0530

    1.updating the ProductComponent,Delete the MockData import 2.in constructor pass the productservice 3.changed the deleteProduct()

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
-import { MockData } from '../mock-data/mock-product-data';
+// import { MockData } from '../mock-data/mock-product-data';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../domain-models/product';
+import { ProductService} from '../service/product.service';
 
 @Component({
   selector: 'app-products',
@@ -9,17 +10,20 @@ import { Product } from '../domain-models/product';
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
-  constructor() { 
-    this.products = MockData.Products;
-  }
-  
-
+  // constructor() { 
+  //   this.products = MockData.Products;
+  // }
+  constructor(public productService: ProductService) {
+    this.products = productService.getProducts();
+    }
   ngOnInit() {
   }
   deleteProduct(product:Product){
-    let index = this.products.indexOf(product);
-if (index !== -1) {
-this.products.splice(index, 1);
-}
+    // let index = this.products.indexOf(product);
+    // if (index !== -1) {
+    // this.products.splice(index, 1);
+    // }
+    this.productService.removeProduct(product);
+    this.products = this.productService.getProducts();
   }
 }
