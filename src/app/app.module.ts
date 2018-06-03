708136449ed6653ee9df755056c85de7a5eb8033
saswat <saswat333@gmail.com>
2018-06-03 12:07:28 +0530

    1.impport ProductService 2.Add the ProductService in providers array i.e. providers: [ProductService]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ProductService } from './service/product.service';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
