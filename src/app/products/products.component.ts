8311f9e59fbd80c2c6b5985e317408ee27a8aeb9
saswat <saswat333@gmail.com>
2018-06-03 08:36:54 +0530

    created deleteProduct() method

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -16,4 +16,10 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
   }
+  deleteProduct(product:Product){
+    let index = this.products.indexOf(product);
+if (index !== -1) {
+this.products.splice(index, 1);
+}
+  }
 }
