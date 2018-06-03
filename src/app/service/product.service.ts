f3cca49bea3a17df46a53ed4b54f1abf4beb1214
saswat <saswat333@gmail.com>
2018-06-03 12:02:51 +0530

    1.created service called product 2.created getProducts() 3.created removeProduct() 4. import MockData

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {MockData } from '../mock-data/mock-product-data';
+import{ Product  } from '../domain-models/product';
+@Injectable()
+export class ProductService {
+products : Product[]=[];
+  constructor() { 
+this.products=MockData.Products;
+}
+getProducts():Product[]{
+  return this.products;
+}
+removeProduct(product: Product) {
+  let index = this.products.indexOf(product);
+  if (index !== -1) {
+  this.products.splice(index, 1);
+  }
+}
+}
