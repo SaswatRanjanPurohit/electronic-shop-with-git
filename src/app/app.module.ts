8850290992d84d21596f75664fe3c5ef3b3eab74
saswat <saswat333@gmail.com>
2018-06-03 19:33:13 +0530

    1. import { ReactiveFormsModule } from '@angular/forms'; 2.imports: [

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { ProductService } from './service/product.service';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
+import { AddProductModelFormComponent } from './add-product-model-form/add-product-model-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductComponent
+    ProductComponent,
+    AddProductModelFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
