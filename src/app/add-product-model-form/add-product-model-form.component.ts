0e6b55281c8da5ba62b2ec0fd574d2213492f503
saswat <saswat333@gmail.com>
2018-06-05 07:54:26 +0530

    1.import Validators 2.import Product from domain-models 3.wrote validation codes

--- src/app/add-product-model-form/add-product-model-form.component.ts
+++ src/app/add-product-model-form/add-product-model-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import { Validators } from '@angular/forms';
+import { Product } from '../domain-models/product';
 @Component({
   selector: 'app-add-product-model-form',
   templateUrl: './add-product-model-form.component.html',
@@ -18,12 +19,18 @@ export class AddProductModelFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.title = new FormControl();
+//     this.title = new FormControl();
+// this.modelName = new FormControl();
+// this.color = new FormControl();
+// this.productType = new FormControl();
+// this.brand = new FormControl();
+// this.price = new FormControl();
+this.title = new FormControl('', [Validators.required, Validators.minLength(10)]);
 this.modelName = new FormControl();
-this.color = new FormControl();
-this.productType = new FormControl();
-this.brand = new FormControl();
-this.price = new FormControl();
+this.color = new FormControl('', Validators.pattern('[a-zA-Z]*'));
+this.productType = new FormControl('', Validators.required);
+this.brand = new FormControl('', Validators.required);
+this.price = new FormControl('', [Validators.required, Validators.min(1)]);
 this.myForm = new FormGroup({
 'title': this.title,
 'modelName' : this.modelName,
