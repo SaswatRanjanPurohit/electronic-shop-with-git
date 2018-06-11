05280090f46ca6bfe65f0d7ee3738d57e250fbec
saswat <saswat333@gmail.com>
2018-06-11 20:02:32 +0530

    1. imports observable,of  2.move code from constructor to ngOnInit

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import {MockData } from '../mock-data/mock-product-data';
 import{ Product } from '../domain-models/product';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 @Injectable()
 export class ProductService {
 products : Product[]=[];
   constructor() { 
 this.products=MockData.Products;
 }
-getProducts():Product[]{
-  return this.products;
+// getProducts():Product[]{
+//   return this.products;
+// }
+getProducts(): Observable<Product[]> {
+  return of(this.products);
 }
 addProduct(product: Product) {
   this.products.push(product);
   }
+  getProduct(id: number): Observable<Product> {
+    return of(this.products.find( p => p.id === id));
+    }
 removeProduct(product: Product) {
   let index = this.products.indexOf(product);
   if (index !== -1) {
   this.products.splice(index, 1);
   }
 }
-getProduct(id: number) {
-  return this.products.find( p => p.id === id);
-  }
+// getProduct(id: number) {
+//   return this.products.find( p => p.id === id);
+//   }
+
 }
