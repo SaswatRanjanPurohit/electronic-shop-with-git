c312561842f5a609e46d7909a236cea9cbce0974
saswat <saswat333@gmail.com>
2018-06-12 07:04:18 +0530

    1.import various HTTP 2.inject HttpClient in constructor

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -3,18 +3,28 @@ import {MockData } from '../mock-data/mock-product-data';
 import{ Product } from '../domain-models/product';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable()
 export class ProductService {
 products : Product[]=[];
-  constructor() { 
+productsUrl = 'api/products';
+  constructor(private httpClient: HttpClient) { 
 this.products=MockData.Products;
 }
 // getProducts():Product[]{
 //   return this.products;
 // }
+// getProducts(): Observable<Product[]> {
+//   return of(this.products);
+// }
 getProducts(): Observable<Product[]> {
-  return of(this.products);
-}
+  return this.httpClient.get<Product[]>(this.productsUrl);
+  .pipe(
+    catchError(this.handleError('getProducts', []))
+    );
+  }
 addProduct(product: Product) {
   this.products.push(product);
   }
